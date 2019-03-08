c14abb92269c56aa5853ae3d9ea240b44264d9c2
sw333zy <sw333zy@gmail.com>
2019-03-08 16:02:33 -0500

    getting ready for heroku needs search

--- src/main/java/com/example/demo/CategoryRepository.java
+++ src/main/java/com/example/demo/CategoryRepository.java
@@ -2,5 +2,7 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 }
